8abfd1166edfe17accf2dc418116b7610f4faa69
Trent Nelson <trent@trent.me>
2018-04-15 13:05:31 -0400

    Remove trailing whitespace.

--- home.js
+++ home.js
@@ -60,6 +60,6 @@ $(function() {
     initTabBox('.tab-box.language.box-simple');
     initTabBox('.tab-box.language.box-string-table');
     initTabBox('.tab-box.language.box-is-prefix-v1');
-    initTabBox('.tab-box.language.box-is-prefix-asm-v1');    
+    initTabBox('.tab-box.language.box-is-prefix-asm-v1');
     //beginLanguageTicker();
 });
