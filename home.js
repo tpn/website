9e84a0f6ecf61c821b19519809e9d091f0fbc1f9
Trent Nelson <trent@trent.me>
2018-04-15 22:51:22 -0400

    Checkpoint commit of latest is-prefix-of-string-in-table article progress.

--- home.js
+++ home.js
@@ -55,11 +55,3 @@ function beginLanguageTicker() {
     }, 200);
 }
 
-$(function() {
-    setupSmoothAnchorScolling();
-    initTabBox('.tab-box.language.box-simple');
-    initTabBox('.tab-box.language.box-string-table');
-    initTabBox('.tab-box.language.box-is-prefix-v1');
-    initTabBox('.tab-box.language.box-is-prefix-asm-v1');
-    //beginLanguageTicker();
-});
---
+++ is-prefix-of-string-in-table/page.js
@@ -0,0 +1,10 @@
+
+$(function() {
+    setupSmoothAnchorScolling();
+    initTabBox('.tab-box.language.box-simple');
+    initTabBox('.tab-box.language.box-string-table');
+    initTabBox('.tab-box.language.box-is-prefix-v1');
+    initTabBox('.tab-box.language.box-is-prefix-asm-v1');
+    initTabBox('.tab-box.language.box-typedefs');
+    //beginLanguageTicker();
+});
