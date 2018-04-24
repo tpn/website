4378be077879edff1dde95eee7c04270a0820f84
Trent Nelson <trent@trent.me>
2018-04-23 20:37:24 -0400

    Checkpoint commit.

--- is-prefix-of-string-in-table/page.js
+++ is-prefix-of-string-in-table/page.js
@@ -6,6 +6,8 @@ $(function() {
     initTabBox('.tab-box.language.box-string-table');
     initTabBox('.tab-box.language.box-is-prefix-v1');
     initTabBox('.tab-box.language.box-is-prefix-asm-v1');
+    initTabBox('.tab-box.language.box-3v2');
+    initTabBox('.tab-box.language.box-x64-5v2');
     initTabBox('.tab-box.language.box-typedefs');
     //beginLanguageTicker();
 });
