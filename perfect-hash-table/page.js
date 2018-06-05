c3717764418afb28f3774cfcbe93e49c5a4c6e64
Trent Nelson <trent@trent.me>
2018-06-05 18:02:21 -0400

    Checkpoint commit.

--- perfect-hash-table/page.js
+++ perfect-hash-table/page.js
@@ -3,4 +3,5 @@ $(function() {
     //setupSmoothAnchorScolling();
     //initTabBox('.tab-box.language.box-intro');
     initTabBox('.tab-box.language.box-table-functions');
+    initTabBox('.tab-box.language.box-chm01-structs');
 });
